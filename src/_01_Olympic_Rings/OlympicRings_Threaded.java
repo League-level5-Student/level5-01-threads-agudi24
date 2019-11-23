bb443d7d4a1e497c8db8141007aa74b32dc29840
League Student <league@iMac-11.attlocal.net>
2019-11-23 11:24:02 -0800

    Update OlympicRings_Threaded.java

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,67 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+	Robot r1 = new Robot(400 - 50, 300);
+	Robot r2 = new Robot(800 - 50, 300);
+	Robot r3 = new Robot(1200 - 50, 300);
+	Robot r4 = new Robot(600 - 50, 500);
+	Robot r5 = new Robot(1000 - 50, 500);
+	int speed = 5000;
+	int ringRadius = 3;
+	r1.setSpeed(20);
+	r2.setSpeed(20);
+	r3.setSpeed(20);
+	r4.setSpeed(20);
+	r5.setSpeed(20);
+	r1.setPenColor(Color.BLUE);
+	r2.setPenColor(Color.BLACK);
+	r3.setPenColor(Color.RED);
+	r4.setPenColor(Color.YELLOW);
+	r5.setPenColor(Color.GREEN);
+	r1.penDown();
+	r2.penDown();
+	r3.penDown();
+	r4.penDown();
+	r5.penDown();
+	r1.setPenWidth(25);
+	r2.setPenWidth(25);
+	r3.setPenWidth(25);
+	r4.setPenWidth(25);
+	r5.setPenWidth(25);
+	for (int i = 1; i < 361; i++) {
+		r1.move(ringRadius);
+		r2.move(ringRadius);
+		r3.move(ringRadius);
+		r4.move(ringRadius);
+		r5.move(ringRadius);
+		r1.turn(1);
+		r2.turn(1);
+		r3.turn(1);
+		r4.turn(1);
+		r5.turn(1);
+	}
+	r1.penUp();
+	r2.penUp();
+	r3.penUp();
+	r4.penUp();
+	r5.penUp();
+	Thread robo1 = new Thread(()->r1.moveTo(10, 10));
+	Thread robo2 = new Thread(()->r2.moveTo(10, 10));
+	Thread robo3 = new Thread(()->r3.moveTo(10, 10));
+	Thread robo4 = new Thread(()->r4.moveTo(10, 10));
+	Thread robo5 = new Thread(()->r5.moveTo(10, 10));
+	robo1.start();
+	robo2.start();
+	robo3.start();
+	robo4.start();
+	robo5.start();
+	}
 
 }
 
