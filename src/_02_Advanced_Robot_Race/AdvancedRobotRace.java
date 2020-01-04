5fe93c227d88d84f514c3e062fdc50a49e5d0280
League Student <league@iMac-13.attlocal.net>
2020-01-04 11:25:41 -0800

    RobotRace, ReverseGreeting (IP)

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -13,120 +13,86 @@ public class AdvancedRobotRace {
 	public static void main(String[] args) throws AWTException {
 		
 	Robot[] robo = new Robot[5];
-	int i = 0;
-	int y = 550;
-	int y2 = 550;
-	int y3 = 550;
-	int y4 = 550;
-	int y5 = 550;
+	Random rand1 = new Random();
+	Random rand2 = new Random();
+	Random rand3 = new Random();
+	Random rand4 = new Random();
+	Random rand5 = new Random();
+	Robot r1 = new Robot();
+	Robot r2 = new Robot();
+	Robot r3 = new Robot();
+	Robot r4 = new Robot();
+	Robot r5 = new Robot();
+	r1.setSpeed(20);
+	r2.setSpeed(20);
+	r3.setSpeed(20);
+	r4.setSpeed(20);
+	r5.setSpeed(20);
 	Thread t = new Thread(()->{
-		Random rand1 = new Random();
-		Robot r1 = new Robot();
 		robo[0] = r1;
 		robo[0].moveTo(100, 550);
-		while(robo[0].getY() > 0) {
-			robo[0].move(rand1.nextInt(50));
-		}
-	});
-	Thread t2 = new Thread(()->{
-		Random rand2 = new Random();
-		Robot r2 = new Robot();
 		robo[1] = r2;
 		robo[1].moveTo(200, 550);
-		while(robo[1].getY() > 0) {
-			robo[1].move(rand2.nextInt(50));
-		}
-	});
-	Thread t3 = new Thread(()->{
-		Random rand3 = new Random();
-		Robot r3 = new Robot();
 		robo[2] = r3;
 		robo[2].moveTo(300, 550);
-		while(robo[2].getY() > 0) {
-			robo[2].move(rand3.nextInt(50));
-		}
-	});
-	Thread t4 = new Thread(()->{
-		Random rand4 = new Random();
-		Robot r4 = new Robot();
 		robo[3] = r4;
 		robo[3].moveTo(400, 550);
-		while(robo[3].getY() > 0) {
-			robo[3].move(rand4.nextInt(50));
-		}
-	});
-	Thread t5 = new Thread(()->{
-		Random rand5 = new Random();
-		Robot r5 = new Robot();
 		robo[4] = r5;
 		robo[4].moveTo(500, 550);
-		while(robo[4].getY() > 0) {
-			robo[4].move(rand5.nextInt(50));
-		}
+	
 	});
 	t.start();
-	t2.start();
-	t3.start();
-	t4.start();
-	t5.start();
-		/*
-		 * for (i = 0; i < robo.length; i++) { robo[i] = new Robot();
-		 * robo[i].setSpeed(10); robo[i].setX(200); robo[i].setY(y); robo[i].turn(90);
-		 * robo[i].move(100 * i); robo[i].turn(-90);
-		 * 
-		 * }
-		 */
-		/*
-		 * while(y > 0 && y2 > 0 && y3 > 0 && y4 > 0 && y5 > 0) { robo[0].move(moveNum);
-		 * robo[1].move(moveNum2); robo[2].move(moveNum3); robo[3].move(moveNum4);
-		 * robo[4].move(moveNum5); y-=moveNum; y2-=moveNum2; y3-=moveNum3; y4-=moveNum4;
-		 * y5-=moveNum5;
-		 * 
-		 * }
-		 */
+	
+	while(r1.getY() > 0 && r2.getY() > 0 && r3.getY() > 0 && r4.getY() > 0 && r5.getY() > 0) {
+		r1.move(rand1.nextInt(50));
+		r2.move(rand2.nextInt(50));
+		r3.move(rand3.nextInt(50));
+		r4.move(rand4.nextInt(50));
+		r5.move(rand5.nextInt(50));
+	}
 
-	if(y < 0 && y2 > 0 && y3 > 0 && y4 > 0 && y5 > 0 ) {
+	if(r1.getY() < 0 && r2.getY() > 0 && r3.getY() > 0 && r4.getY() > 0 && r5.getY() > 0) {
 		JOptionPane.showMessageDialog(null, "Robot #1 Wins!");
-		robo[0].setX(300);
-		robo[0].setY(300);
+		r1.setX(300);
+		r1.setY(300);
 		for (int j = 0; j < 1;) {
-			robo[0].turn(360);
+			r1.turn(360);
 		}
 		
 	}
-	if(y > 0 && y2 < 0 && y3 > 0 && y4 > 0 && y5 > 0 ) {
+	if(r1.getY() > 0 && r2.getY() < 0 && r3.getY() > 0 && r4.getY() > 0 && r5.getY() > 0) {
 		JOptionPane.showMessageDialog(null, "Robot #2 Wins!");
-		robo[1].setX(300);
-		robo[1].setY(300);
+		r2.setX(300);
+		r2.setY(300);
 		for (int j = 0; j < 1;) {
-			robo[1].turn(360);
+			r2.turn(360);
 		}
 		
 	}
-	if(y > 0 && y2 > 0 && y3 < 0 && y4 > 0 && y5 > 0 ) {
+	if(r1.getY() > 0 && r2.getY() > 0 && r3.getY() < 0 && r4.getY() > 0 && r5.getY() > 0) {
 		JOptionPane.showMessageDialog(null, "Robot #3 Wins!");
-		robo[2].setX(300);
-		robo[2].setY(300);
+		r3.setX(300);
+		r3.setY(300);
 		for (int j = 0; j < 1;) {
-			robo[2].turn(360);
+			r3.turn(360);
 		}
 		
 	}
-	if(y > 0 && y2 > 0 && y3 > 0 && y4 < 0 && y5 > 0 ) {
+	if(r1.getY() > 0 && r2.getY() > 0 && r3.getY() > 0 && r4.getY() < 0 && r5.getY() > 0) {
 		JOptionPane.showMessageDialog(null, "Robot #4 Wins!");
-		robo[3].setX(300);
-		robo[3].setY(300);
+		r4.setX(300);
+		r4.setY(300);
 		for (int j = 0; j < 1;) {
-			robo[3].turn(360);
+			r4.turn(360);
 		}
 		
 	}
-	if(y > 0 && y2 > 0 && y3 > 0 && y4 > 0 && y5 < 0 ) {
+	if(r1.getY() > 0 && r2.getY() > 0 && r3.getY() > 0 && r4.getY() > 0 && r5.getY() < 0) {
 		JOptionPane.showMessageDialog(null, "Robot #5 Wins!");
-		robo[4].setX(300);
-		robo[4].setY(300);
+		r5.setX(300);
+		r5.setY(300);
 		for (int j = 0; j < 1;) {
-			robo[4].turn(360);
+			r5.turn(360);
 		}
 		
 	}
--- src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
+++ src/_03_Threaded_Reverse_Greeting/ThreadedReverseGreeting.java
@@ -1,6 +1,29 @@
 package _03_Threaded_Reverse_Greeting;
 
 public class ThreadedReverseGreeting {
+	int numThreads = 1;
+	public static void main(String[] args) {
+		ThreadedReverseGreeting main = new ThreadedReverseGreeting();
+	} 
+	
+	public ThreadedReverseGreeting() {
+		Thread starter = new Thread(()->threader());
+		starter.start();
+		//System.out.println("test");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void threader(/*Thread threadee*/) {
+		//System.out.println(numThreads);
+		
+		if(numThreads < 50) {
+			Thread nextThread = new Thread(() -> threader());
+			nextThread.start();
+		}
+
+		System.out.println("Hello from thread " + numThreads);
+		numThreads++;
+	}
   //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
