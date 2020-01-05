c0f61069f3f0d9018a732825c39c072ad7ab7eb9
League Student <league@iMac-12.attlocal.net>
2020-01-05 10:40:47 -0800

    FInished module

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,35 @@
+  
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class ThreadPool {
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQ;
+	
+	ThreadPool(int t) {
+		threads = new Thread[t];
+		taskQ = new ConcurrentLinkedQueue<Task>();
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Worker(taskQ));
+		}
+	}
+	
+	//there are 4 threads in the thread pool
+	
+	void addTask(Task t) {
+		taskQ.add(t);
+	}
+	
+	void start() {
+		for (Thread t: threads) {
+			t.run();
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
